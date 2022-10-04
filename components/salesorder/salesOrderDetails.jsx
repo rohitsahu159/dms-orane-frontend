c2fc998314ae1ffa5ba780468aa72af53cec7de7
Rudraksh Pahwa <Pahwarudraksh17@gmail.com>
2022-10-04 10:59:28 +0530

    wips

--- components/salesorder/salesOrderDetails.jsx
+++ components/salesorder/salesOrderDetails.jsx
@@ -142,6 +142,7 @@ const SalesOrderDetail = ({ route }) => {
                             </Card.Content>
 
                         </Card>
+                        
 
                     </>)
                 }
